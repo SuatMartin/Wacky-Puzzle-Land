4163f14bb3dfe7a06b16c6cc830978e313d3c11a
Suat Martin <sgiraygonzal@umass.edu>
2023-12-12 13:29:26 -0500

    wrong button swiches scenes

---
+++ Assets/Scripts/InteractWrong.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteractWrong : MonoBehaviour
+{
+    public GameObject FirstPersonController;
+
+    private void Update()
+    {
+        if (isPlayerOnTop() && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleObjectState();
+        }
+    }
+
+    private void ToggleObjectState()
+    {
+        SceneManager.LoadScene("topleft");
+    }
+
+     private bool isPlayerOnTop()
+    {
+        if (FirstPersonController == null)
+        {
+            Debug.LogError("FirstPersonController reference not set in the inspector!");
+            return false;
+        }
+
+        float xThreshold = 50;
+        float zThreshold = 50;
+        
+        Vector3 playerPosition = FirstPersonController.transform.position;
+        Debug.Log(transform.position.x +" " +playerPosition.x);
+        Debug.Log(transform.position.z +" " +playerPosition.z);
+        if (Mathf.Abs(transform.position.x - playerPosition.x) < xThreshold &&
+               Mathf.Abs(transform.position.z - playerPosition.z) < zThreshold){
+                Debug.Log("standing on it");
+                return true;
+        } else {
+            return false;
+        }
+    }
+}
